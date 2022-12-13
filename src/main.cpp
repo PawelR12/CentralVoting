be2ffa56998c1b56447a75b4595406df8605cf5f
Pawel1218 <pawel.rapta12@gmail.com>
2022-12-13 19:07:17 +0100

    before checking

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@
 
 #define ACTIVATE_BUTTON 4
 #define ACTION_BUTTON 15
-#define GREEN_LED 34
-#define YELLOW_LED 33
-#define RED_LED 32
+#define GREEN_LED 25
+#define YELLOW_LED 26
+#define RED_LED 27
 //Add WIFI data
 #define WIFI_SSID "Redmi Note 9"
 #define WIFI_PASSWORD "1234abcd"
@@ -48,7 +48,11 @@ void http_send(String data);
 
 void setup() {
 
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
   // Set serial communication and communication with LoRa transceiver
+  digitalWrite(YELLOW_LED, HIGH);
   Serial.begin(115200);
   LoRa.setPins(ss, rst, dio0);
   while (!LoRa.begin(866E6)){
@@ -74,48 +78,50 @@ void setup() {
   // HTTP Data Setup
   Serial.println("--->First HTTP Data Setup");
   
-  //   if(wifiMulti.run() == WL_CONNECTED){
-  //     HTTPClient http;
-  //     http.begin("https://wirelessvoting.000webhostapp.com/index.php");   //Indicate the destination webpage 
-  //     http.addHeader("Content-Type", "application/x-www-form-urlencoded");          //Prepare the header
+    if(wifiMulti.run() == WL_CONNECTED){
+      HTTPClient http;
+      http.begin("https://wirelessvoting.000webhostapp.com/index.php");   //Indicate the destination webpage 
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");          //Prepare the header
       
-  //     data_to_send =  "change_stage";
-  //     tmp = data_to_send + String(protocol.voting);
-  //     Serial.println("--->Change voting status over wifi");
-  //     http.POST(tmp);
+      data_to_send =  "change_stage_";
+      tmp = data_to_send + String(protocol.voting);
+      Serial.println("--->Change voting status over wifi");
+      http.POST(tmp);
       
-  //     tmp =  "send_devices=1";
-  //     //tmp = data_to_send + String(protocol.voting);
-  //     Serial.println("--->Change device number");
-  //     http.POST(tmp);
+      tmp =  "send_devices=1";
+      //tmp = data_to_send + String(protocol.voting);
+      Serial.println("--->Change device number");
+      http.POST(tmp);
 
 
-  //     Serial.println("--->Clearing basic values");
-  //     tmp =  "connected_devices=0";
-  //     //tmp = data_to_send + String(protocol.voting);
-  //     Serial.print("--->  ..conncected devices");
-  //     http.POST(tmp);
-  //     // http.end();
+      Serial.println("--->Clearing basic values");
+      tmp =  "conncected_devices=0";
+      //tmp = data_to_send + String(protocol.voting);
+      Serial.print("--->  ..conncected devices");
+      http.POST(tmp);
+      // http.end();
 
-  //     tmp =  "yes_votes=0";
-  //     //tmp = data_to_send + String(protocol.voting);
-  //     Serial.print("  .. yes_votes");
-  //     http.POST(tmp);
-  //     // http.end();
+      tmp =  "yes_votes=0";
+      //tmp = data_to_send + String(protocol.voting);
+      Serial.print("  .. yes_votes");
+      http.POST(tmp);
+      // http.end();
 
-  //     tmp =  "no_votes=0";
-  //     //tmp = data_to_send + String(protocol.voting);
-  //     Serial.print("  .. no_votes");
-  //     http.POST(tmp);
-  //     // http.end();
+      tmp =  "no_votes=0";
+      //tmp = data_to_send + String(protocol.voting);
+      Serial.print("  .. no_votes");
+      http.POST(tmp);
+      // http.end();
 
-  //     tmp =  "no_decision_votes=0";
-  //     //tmp = data_to_send + String(protocol.voting);
-  //     Serial.println("  .. no_decision");
-  //     http.POST(tmp);
-  //     http.end();
-  // }
+      tmp =  "no_decision_votes=0";
+      //tmp = data_to_send + String(protocol.voting);
+      Serial.println("  .. no_decision");
+      http.POST(tmp);
+      http.end();
+  }
  
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(GREEN_LED, HIGH);
   Serial.println("You can start");
 }
 
@@ -124,12 +130,16 @@ void loop() {
 
   if(digitalRead(ACTION_BUTTON) && protocol.voting == voting_is_close)
   {
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(YELLOW_LED, HIGH);
       protocol.clear_votes();
       http_send("change_stage_0");
-      http_send("connected_devices=0");
+      http_send("conncected_devices=0");
       encryption_key = (rand() % 5) + 1;
       protocol.send_voting_open(0, encryption_key);
       protocol.voting = voting_is_open;
+      digitalWrite(YELLOW_LED, LOW);
+      digitalWrite(GREEN_LED, HIGH);
       Serial.println("---- START VOTING ----");
     
   }
@@ -150,17 +160,23 @@ void loop() {
     if(digitalRead(ACTION_BUTTON) && protocol.voting == voting_is_open && stage == 0){
         if(stage == 0){
           stage = 1;
+          digitalWrite(GREEN_LED, LOW);
+          digitalWrite(YELLOW_LED, HIGH);
           Serial.println("--->Server communication");
           http_send("change_stage_2");
           data_to_send =  "connected_devices=" + String(protocol.connected_devices);
           http_send(data_to_send);
           protocol.send_can_vote(0);
           // protocol.voting == during_voting;
+          digitalWrite(GREEN_LED, HIGH);
+          digitalWrite(YELLOW_LED, LOW);
           Serial.println("You can send the voices");
           delay(200);
         }
     }
     if(digitalRead(ACTION_BUTTON) && stage == 1){
+          digitalWrite(GREEN_LED, HIGH);
+          digitalWrite(YELLOW_LED, LOW);
           protocol.send_close_voting();
           Serial.println("--- VOTING IS CLOSE");    
           Serial.println("--->Server communication");
@@ -177,6 +193,8 @@ void loop() {
           http_send("change_stage_1");
       
           stage = 0;
+          digitalWrite(GREEN_LED, HIGH);
+          digitalWrite(YELLOW_LED, LOW);
           Serial.println("You can clear");
         }
 }
